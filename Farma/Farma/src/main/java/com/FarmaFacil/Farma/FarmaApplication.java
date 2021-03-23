90b310c33b004935f2ce255e40800ef8d2a0b61d
Yuri <yuri.rampazo@outlook.com>
2021-03-23 13:35:32 -0300

    Task1

---
+++ Farma/Farma/src/main/java/com/FarmaFacil/Farma/FarmaApplication.java
@@ -0,0 +1,13 @@
+package com.FarmaFacil.Farma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaApplication.class, args);
+	}
+
+}
---
+++ Farma/Farma/src/main/java/com/FarmaFacil/Farma/repository/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.FarmaFacil.Farma.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.FarmaFacil.Farma.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+	public List<Categoria> findAllByDescricaoContainingIgnoreCase(String nome);
+}
